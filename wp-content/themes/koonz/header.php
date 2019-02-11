d7801c5febd8f9c9ef2f725e33318ac284bb1259
Garrett Cullen <garrettwcullen@gmail.com>
2019-02-11 15:20:23 -0800

    push

--- wp-content/themes/koonz/header.php
+++ wp-content/themes/koonz/header.php
@@ -27,14 +27,13 @@
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
-
+<link rel="stylesheet" href="https://use.typekit.net/wde8ipm.css">
 
 <style>
 
 
 <?php the_field( 'review_css','option'); ?>
 
-@import url("https://use.typekit.net/wde8ipm.css");
 
 </style>
 
@@ -69,7 +68,7 @@
 					
 					<span class="location_title">District of Columbia</span><!-- location_title -->
 				
-					<a class="phone" href="">(202) 796-3291</a><!-- location_title -->
+					<a class="phone" href="tel:2027963291">(202) 796-3291</a><!-- location_title -->
 					
 				</div><!-- phone_row -->
 				
@@ -77,7 +76,7 @@
 					
 					<span class="location_title">Virginia</span><!-- location_title -->
 				
-					<a class="phone" href="">(703) 498-2928</a><!-- location_title -->
+					<a class="phone" href="tel:7034982928">(703) 498-2928</a><!-- location_title -->
 					
 				</div><!-- phone_row -->
 				
@@ -85,23 +84,39 @@
 					
 					<span class="location_title">Maryland</span><!-- location_title -->
 				
-					<a class="phone" href="">(301) 637-4614</a><!-- location_title -->
+					<a class="phone" href="tel:3016374614">(301) 637-4614</a><!-- location_title -->
 					
 				</div><!-- phone_row -->
 				
 			</div><!-- phone_wrapper -->
 			
-			<a class="se_habla" href="">Se habla Espanol</a><!-- se_habla -->
+			<a class="se_habla" href="">
+				
+				<span class="se_habla_title">se habla espaÑol </span><!-- se_habla_title -->
+				
+				<span class="se_habla_line"></span><!-- se_habla_line -->
+			
+			</a><!-- se_habla -->
 			
 		</div><!-- header_middle -->
 		
-		<div class="header_right"></div><!-- header_right -->
+		<div class="header_right">
+			
+			<a class="" href="tel:2027963291">
+					
+				<?php echo file_get_contents("wp-content/themes/koonz/images/content01_icon-01.svg"); ?>
+				
+				<span class="call_today">Call today for your free consultation</span><!-- call_today -->
+				
+			</a>
+			
+		</div><!-- header_right -->
 		
 	</header>
 	
 	<nav>
 		
-		<?php // wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'main_menu' ) ); ?>
+		<?php wp_nav_menu( array( 'container_class' => 'menu-header', 'theme_location' => 'main_menu' ) ); ?>
 		
 	</nav>
 				
--- wp-content/themes/koonz/page-templates/homepage_template_parts/section-1.php
+++ wp-content/themes/koonz/page-templates/homepage_template_parts/section-1.php
@@ -1,6 +1,6 @@
 <section id="section_one">
 	
-		
+		<img src="<?php bloginfo('template_directory');?>/images/hero_img_desktop.jpg"/>
 
 	
 </section><!-- section_one -->
