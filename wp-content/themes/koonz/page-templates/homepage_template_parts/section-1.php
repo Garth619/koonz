0b30fcbd53a9477e6ef405f6254494e395c59058
Garrett Cullen <garrettwcullen@gmail.com>
2019-02-12 08:38:42 -0800

    push

--- wp-content/themes/koonz/page-templates/homepage_template_parts/section-1.php
+++ wp-content/themes/koonz/page-templates/homepage_template_parts/section-1.php
@@ -1,7 +1,26 @@
 <section id="section_one">
 	
-		<img src="<?php bloginfo('template_directory');?>/images/hero_img_desktop.jpg"/>
+		<img class="hero" src="<?php bloginfo('template_directory');?>/images/hero_img_desktop.jpg"/>
+		
+		<div class="sec_one_inner_content">
+			
+			<div class="sec_one_video_wrapper">
+				
+				<div class="play_button_wrapper">
+					
+					<div class="play_circle"></div><!-- play_circle -->
+					
+				</div><!-- play_button_wrapper -->
+				
+				<span class="video_cta">Watch Our Video To LEarn About Us</span><!-- video_cta -->
+				
+			</div><!-- sec_one_video_wrapper -->
+			
+			<span class="sec_one_title">Experienced at winning cases for people who need justice</span><!-- sec_one_title -->
+			
+			<a class="button sec_one_button" href=""></a><!-- button -->
+			
+		</div><!-- sec_one_inner_content -->
 
-	
 </section><!-- section_one -->
 
