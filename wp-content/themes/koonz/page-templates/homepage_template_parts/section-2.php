6b122fb343d243860596d6892861b71191413f1c
Garrett Cullen <garrettwcullen@gmail.com>
2019-02-13 10:16:59 -0800

    push

--- wp-content/themes/koonz/page-templates/homepage_template_parts/section-2.php
+++ wp-content/themes/koonz/page-templates/homepage_template_parts/section-2.php
@@ -118,6 +118,73 @@
 		</div><!-- selling_point_box -->
 				
 	</div><!-- selling_points_wrapper -->
+	
+	
+	<div class="sec_two_content_wrapper">
+		
+		<h1 class="sec_two_header">Washington DC,<br/> Virginia and Maryland Personal Injury Lawyers</h1><!-- sec_two_header -->
+		
+		<span class="sec_two_intro">Our law firm is one of the largest, most experienced and most respected plaintiff’s personal injury law firms in the region</span><!-- sec_two_intro -->
+		
+		<div class="sec_two_container">
+			
+			<div class="sec_two_content content">
+				
+				<p>For more than 35 years, the Washington, DC, Virginia and Maryland and personal injury lawyers of Koonz, McKenney, Johnson, DePaolis & Lightfoot, L.L.P.  have successfully obtained substantial monetary compensation on behalf of individuals who have been injured or for families of those killed due to the negligence or wrongdoing of others. Our law firm is one of the largest, most experienced and most respected plaintiff’s personal injury law firms in the region. In fact, our recognitions include four of the firm’s attorneys being selected as Top Lawyers by Washingtonian Magazine, five chosen by their peers for inclusion in Washington, DC Super Lawyers and five of our personal injury attorneys being selected for inclusion in Best Lawyers in America©.</p> 
+
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. If you have been injured, have been denied an insurance claim, or if your loved one passed away wrongfully, contact one of our attorneys. We will fight for your justice and help you move forward.  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum.</p>
+				
+			</div><!-- sec_two_content -->
+			
+			<div class="sec_two_sidebar">
+				
+				<span class="sec_two_sidebar_title">Let our attorneys fight for you</span><!-- sec_two_sidebar_title -->
+				
+				<ul>
+					<li>
+						<a class="" href="">automobile Accidents</a>
+					</li>
+					<li>
+						<a class="" href="">Medical Malpractice</a>
+					</li>
+					<li>
+						<a class="" href="">Workers’ Compensation</a>
+					</li>
+					<li>
+						<a class="" href="">Traumatic Brain Injuries</a>
+					</li>
+					<li>
+						<a class="" href="">Pedestrian Accidents</a>
+					</li>
+					<li>
+						<a class="" href="">Injuries to Children</a>
+					</li>
+					<li>
+						<a class="" href="">Construction Accidents</a>
+					</li>
+					<li>
+						<a class="" href="">Spinal Cord Injuries</a>
+					</li>
+					<li>
+						<a class="" href="">trucking collisions</a>
+					</li>
+					<li>
+						<a class="" href="">asbestos claims </a>
+					</li>
+					<li>
+						<a class="" href="">mesothelioma cases</a>
+					</li>
+				</ul>
+				
+				
+				<a class="view_all_button" href="">View All Practice Areas</a><!-- view_all_button -->
+				
+			</div><!-- sec_two_sidebar -->
+			
+		</div><!-- sec_two_container -->
+		
+		
+	</div><!-- sec_two_content_wrapper -->
 
 	
 </section><!-- section_two -->
