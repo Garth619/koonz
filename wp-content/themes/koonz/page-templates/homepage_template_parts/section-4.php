49bf912157cd02a0b0f13f2223fe8728b875962e
Garrett Cullen <garrettwcullen@gmail.com>
2019-02-13 15:26:04 -0800

    push

---
+++ wp-content/themes/koonz/page-templates/homepage_template_parts/section-4.php
@@ -0,0 +1,59 @@
+<section id="section_four">
+	
+	<div class="sec_four_inner">
+		
+		<div class="sec_four_left">
+			
+			<span class="sec_four_title">Case Results</span><!-- sec_four_title -->
+			
+			<span class="sec_four_intro">We are committed to each and every client who retains us, regardless of their cases' size.</span><!-- sec_four_intro -->
+			
+			<a class="view_all_button sec_four_button" href="">View All results</a>
+			
+		</div><!-- sec_four_left -->
+		
+		<div class="sec_four_right">
+			
+			
+			<div class="sec_four_slider_wrapper">
+				
+				<div class="sec_four_slider">
+					
+					<div class="sec_four_slide">
+						
+						<div class="sec_four_slide_reg">
+							
+							<?php echo file_get_contents("wp-content/themes/koonz/images/caseresults_icon-01.svg"); ?>
+							
+							<span class="sec_four_amount">$9.3 M</span><!-- sec_four_amount -->
+							
+							<span class="sec_four_description">d.c. pedestrian struck by a vehicle</span><!-- sec_four_description -->
+							
+							<span class="plus_sign">+</span><!-- plus_sign -->
+							
+						</div><!-- sec_four_slide_reg -->
+						
+						<div class="sec_four_slide_hover">
+							
+							<div class="sec_four_hover_inner">
+							
+								<span class="sec_four_hover_title">d.c. pedestrian struck by a vehicle </span><!-- sec_four_hover_title -->
+							
+								<span class="sec_four_hover_content">Roger Johnson and Kasey Murray recently recovered $9,300,000 for a pedestrian who was struck by a vehicle. The pedestrian sustained life-changing crush injuries and is now disabled as a result of the accident. The case involved legal questions regarding the duties of drivers and their employers, as well as the legal impact of the recovery of workers’ compensation benefits on a personal injury lawsuit. The settlement is one of the largest in the firm’s history.</span><!-- sec_four_hover_content -->
+							
+							</div><!-- sec_four_hover_inner -->
+												
+						</div><!-- sec_four_slide_hover -->
+						
+					</div><!-- sec_four_slide -->
+					
+				</div><!-- sec_four_slider -->
+				
+			</div><!-- sec_four_slider_wrapper -->
+			
+			
+		</div><!-- sec_four_right -->
+		
+	</div><!-- sec_four_inner -->
+	
+</section><!-- section_four -->
