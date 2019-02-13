a0a2a73a0dd3f3e39e9296ac0e32f5643d482f53
Garrett Cullen <garrettwcullen@gmail.com>
2019-02-13 13:33:21 -0800

    push

---
+++ wp-content/themes/koonz/page-templates/homepage_template_parts/section-3.php
@@ -0,0 +1,21 @@
+<section id="section_three">
+	
+	<div class="sec_image_wrapper">
+		
+		<img src="<?php bloginfo('template_directory');?>/images/content01_img_01.jpg"/>
+		<img src="<?php bloginfo('template_directory');?>/images/content01_img_02.jpg"/>
+		<img src="<?php bloginfo('template_directory');?>/images/content01_img_03.jpg"/>
+		
+	</div><!-- sec_image_wrapper -->
+	
+	<div class="sec_three_content content">
+		
+		<?php echo file_get_contents("wp-content/themes/koonz/images/content01_icon-01.svg"); ?>
+		
+		<span class="sec_three_title">why choose our maryland, virginia, and washington dc personal injury attorneys?</span><!-- sec_three_title -->
+		
+		<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Nemo enim ipsam voluptatem quia voluptas sit aspernatur.</p>
+		
+	</div><!-- sec_three_content -->
+	
+</section><!-- section_three -->
