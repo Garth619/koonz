2631dad870daaaa498c95cd4e7477b644049a34c
Garrett Cullen <garrettwcullen@gmail.com>
2019-02-14 15:09:42 -0800

    push

--- wp-content/themes/koonz/footer.php
+++ wp-content/themes/koonz/footer.php
@@ -1,5 +1,11 @@
 <footer id="footer_trigger">
 	
+	<div class="footer_inner">
+		
+		<span class="footer_title"></span>
+		
+	</div><!-- footer_inner -->
+	
 	<?php // gravity_form(1, false, false, false, '', true, 12); ?>
 		
 </footer>
