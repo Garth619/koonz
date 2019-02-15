bbda3a4cf2a8e83d982eacc46acfcb865f4256e3
Garrett Cullen <garrettwcullen@gmail.com>
2019-02-15 08:40:42 -0800

    push

--- wp-content/themes/koonz/footer.php
+++ wp-content/themes/koonz/footer.php
@@ -2,11 +2,19 @@
 	
 	<div class="footer_inner">
 		
-		<span class="footer_title"></span>
+		<span class="footer_title">request your<br/> free consultation</span>
+		
+		<span class="footer_intro">We do not charge for an initial consultation and collects no fees unless compensation is recovered for you. </span><!-- footer_intro -->
+		
+		<div class="form_wrapper">
+			
+			<?php gravity_form(1, false, false, false, '', true, 12); ?>
+			
+		</div><!-- form_wrapper -->
 		
 	</div><!-- footer_inner -->
 	
-	<?php // gravity_form(1, false, false, false, '', true, 12); ?>
+	
 		
 </footer>
 
