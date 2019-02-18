c7521cd5cd63f7778c6157aa684f9a0a6b38a754
Garrett Cullen <garrettwcullen@gmail.com>
2019-02-18 15:29:35 -0800

    push

--- wp-content/themes/koonz/page-templates/template-testimonials.php
+++ wp-content/themes/koonz/page-templates/template-testimonials.php
@@ -5,6 +5,73 @@
 get_header(); ?>
 
 
+<div id="internal_main">
+
+<div class="outer_container">
+	
+		<h1 class="internal_header"><?php the_title();?></h1><!-- page_title -->
+	
+		<div class="testimonials_wrapper">
+		
+			<div class="testi_col">
+				
+				<div class="single_testimonial">
+					
+					<?php echo file_get_contents("wp-content/themes/koonz/images/test-stars.svg"); ?>
+					
+					<span class="testi_header">Nulla elementum semligula set aliquet, at rutrum turpis</span><!-- testi_header -->
+					
+					<span class="testi_content">Aenean nec rhoncus velit. Phasellus eget faucibus justo, eget mattis lacus. Mauris in eros hendrerit, lacinia velit quis, rutrum dui. Duis et tempus leo. Mauris nibh orci, ullamcorper vitae commodo in, semper id nisl. Pellentesque dictum ut nunc sit amet sagittis. Quisque sed interdum ipsum. Maecenas eu efficitur odio, quis rutrum erat. Fusce pellentesque diam elit, nec sagittis massa feugiat in.</span><!-- testi_content -->
+					
+					<span class="testi_name">D. Fox</span><!-- testi_name -->
+					
+				</div><!-- single_testimonial -->
+				
+				<div class="single_testimonial">
+					
+					<?php echo file_get_contents("wp-content/themes/koonz/images/test-stars.svg"); ?>
+					
+					<span class="testi_header">Nulla elementum semligula set aliquet, at rutrum turpis</span><!-- testi_header -->
+					
+					<span class="testi_content">Aenean nec rhoncus velit. Phasellus eget faucibus justo, eget mattis lacus. Mauris in eros hendrerit, lacinia velit quis, rutrum dui. Duis et tempus leo. Mauris nibh orci, ullamcorper vitae commodo in, semper id nisl. Pellentesque dictum ut nunc sit amet sagittis. Quisque sed interdum ipsum. Maecenas eu efficitur odio, quis rutrum erat. Fusce pellentesque diam elit, nec sagittis massa feugiat in.</span><!-- testi_content -->
+					
+					<span class="testi_name">D. Fox</span><!-- testi_name -->
+					
+				</div><!-- single_testimonial -->
+				
+			</div><!-- testi_col -->
+			
+			<div class="testi_col">
+				
+				<div class="single_testimonial">
+					
+					<?php echo file_get_contents("wp-content/themes/koonz/images/test-stars.svg"); ?>
+					
+					<span class="testi_header">Nulla elementum semligula set aliquet, at rutrum turpis</span><!-- testi_header -->
+					
+					<span class="testi_content">Aenean nec rhoncus velit. Phasellus eget faucibus justo, eget mattis lacus. Mauris in eros hendrerit, lacinia velit quis, rutrum dui. Duis et tempus leo. Mauris nibh orci, ullamcorper vitae commodo in, semper id nisl. Pellentesque dictum ut nunc sit amet sagittis. Quisque sed interdum ipsum. Maecenas eu efficitur odio, quis rutrum erat. Fusce pellentesque diam elit, nec sagittis massa feugiat in.</span><!-- testi_content -->
+					
+					<span class="testi_name">D. Fox</span><!-- testi_name -->
+					
+				</div><!-- single_testimonial -->
+				
+				<div class="single_testimonial">
+					
+					<?php echo file_get_contents("wp-content/themes/koonz/images/test-stars.svg"); ?>
+					
+					<span class="testi_header">Nulla elementum semligula set aliquet, at rutrum turpis</span><!-- testi_header -->
+					
+					<span class="testi_content">Aenean nec rhoncus velit. Phasellus eget faucibus justo, eget mattis lacus. Mauris in eros hendrerit, lacinia velit quis, rutrum dui. Duis et tempus leo. Mauris nibh orci, ullamcorper vitae commodo in, semper id nisl. Pellentesque dictum ut nunc sit amet sagittis. Quisque sed interdum ipsum. Maecenas eu efficitur odio, quis rutrum erat. Fusce pellentesque diam elit, nec sagittis massa feugiat in.</span><!-- testi_content -->
+					
+					<span class="testi_name">D. Fox</span><!-- testi_name -->
+					
+				</div><!-- single_testimonial -->
+				
+			</div><!-- testi_col -->
+		
+		</div><!-- testimonials_wrapper -->
+		
+</div><!-- internal_main -->
 
 
 <?php get_footer(); ?>
