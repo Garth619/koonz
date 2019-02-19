fa0f203d33632b6571d85d660aa75ace64fe86e1
Garrett Cullen <garrettwcullen@gmail.com>
2019-02-19 13:37:40 -0800

    push

--- wp-content/themes/koonz/page-templates/template-bio.php
+++ wp-content/themes/koonz/page-templates/template-bio.php
@@ -14,9 +14,32 @@ get_header(); ?>
 			
 			<div class="att_container content">
 				
-				<!-- 		<h1 class="att_bio"><?php the_title();?></h1> --><!-- page_title -->
+				<h1 class="att_bio_header"><?php the_title();?></h1><!-- att_bio_header -->
 				
-					<?php //get_template_part( 'loop', 'page' ); ?>
+				<span class="att_bio_subheader">Washington, D.C. Injury Lawyer</span><!-- att_bio_subheader -->
+				
+				<h2>Roger C. Johnson founded the law firm of Koonz, McKenney, Johnson, DePaolis & Lightfoot in September of 1979. </h2>
+				
+				<p>He is a graduate of the University of Michigan and the National Law Center of the George Washington University, receiving his Juris Doctor degree in 1977. During 1977 and 1978, he was law clerk to the Honorable George H. Revercomb, who at that time served as a trial judge on the Superior Court of the District of Columbia.</p>
+
+				<p>In his last three trials, Mr. Johnson obtained verdicts or post-trial settlements totaling nearly $16 million on behalf of his clients. He handles serious injury cases.</p>
+				
+				<p>Mr. Johnson was trial counsel in Redd v. Product Development Corporation, in which he obtained the largest settlement in the District of Columbia history for his client, truck accident victim Ethel Jane Redd, who was dragged by a rampaging truck at 16th and K Streets in Washington, D.C.. One year later, he obtained the second largest District of Columbia settlement in the case of Shover v. Stella Steel. This settlement was obtained on behalf of a young construction worker who sustained a severe brain injury while working on the construction of a new building in downtown Washington. Following these two settlements, Mr. Johnson was featured on a front page article in the Style section of The Washington Post entitled “The Man Who Makes Them Pay.”</p>
+
+				<h3>In November 2016, Mr. Johnson settled a pedestrian leg crush case for over $9.3 million.</h3>
+
+				<p>He has obtained million dollar verdicts and settlements for clients in automobile accidents, including pedestrian injuries, medical malpractice cases, construction accident cases, and slip and fall cases, along with many other types of injury claims.</p>
+
+				<p>In 2006, Mr. Johnson was inducted into the International Society of Barristers. In December of 2017, he was again recognized by Washingtonian Magazine as one of the areas “top 1%” of personal injury lawyers. He was also named one of “America’s Best Lawyers” for 2007-2017.</p>
+
+				<p>Mr. Johnson is frequently asked to mediate serious medical malpractice cases and has served as an instructor to the attorney neutral evaluators and mediators who serve in the Multi-Door Dispute Resolution Division of the Superior Court of the District of Columbia. Through this program, Mr. Johnson has taught over 300 volunteer attorneys in seminars on personal injury law as well as mediation techniques. He has taught the same mediation and settlement techniques to the judges of the Civil Division of the Superior Court. Mr. Johnson has also served as a member of the Board of Governors of the District of Columbia Trial Lawyers Association.</p>
+
+				<p>In addition to his professional responsibilities as an attorney, Mr. Johnson is currently a director of the United Bank of Virginia, a $19 billion regional bank. He has been active in the Big Brothers of Washington, D.C. and is a past Big Brother of the Year. He has coached baseball in the Capitol City Little League and girls’ basketball in Montgomery County, Maryland. He lives in Washington, D.C. with his wife Carolyn McKenney. They have three children. </p>
+
+				<p>If you would like to contact Mr. Johnson or another Maryland, Virginia or Washington, D.C. injury attorney, please call him at (202) 794-5416 or fill out a contact form.</p>
+
+				
+				<?php //get_template_part( 'loop', 'page' ); ?>
 				
 			</div><!-- att_container -->
 			
