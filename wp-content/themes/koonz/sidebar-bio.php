09ac464de93e324a9ecbdcb6f92ae302dd082087
Garrett Cullen <garrettwcullen@gmail.com>
2019-02-19 15:37:03 -0800

    push

--- wp-content/themes/koonz/sidebar-bio.php
+++ wp-content/themes/koonz/sidebar-bio.php
@@ -4,7 +4,9 @@
 		
 		<div class="attt_bio_image">
 			
-			<img src="<?php bloginfo('template_directory');?>/images/attorney-photo.jpg"/>
+			<?php $attorney_image = get_field( 'attorney_image' ); ?>
+			
+			<img class="desktop" src="<?php echo $attorney_image['url']; ?>" alt="<?php echo $attorney_image['alt']; ?>"/>
 			
 		</div><!-- attt_bio_image -->
 		
@@ -12,84 +14,102 @@
 			
 			<div class="att_bio_contact_info">
 				
-				<span class="att_bio_contact_title">Email</span><!-- att_bio_contact_title -->
+				<?php if(get_field('att_bio_email')):?>
 				
-				<a class="att_bio_contact" href="mailto:rjohnson@koonz.com">rjohnson@koonz.com</a><!-- att_bio_contact -->
+					<span class="att_bio_contact_title">Email</span><!-- att_bio_contact_title -->
 				
-				<span class="att_bio_contact_title">Phone</span><!-- att_bio_contact_title -->
+					<a class="att_bio_contact" href="mailto:<?php the_field( 'att_bio_email' ); ?>"><?php the_field( 'att_bio_email' ); ?></a><!-- att_bio_contact -->
 				
-				<a class="att_bio_contact" href="tel:2028221870">202-822-1870</a><!-- att_bio_contact -->
+				<?php endif;?>
 				
-				<span class="att_bio_contact_title">Assistant</span><!-- att_bio_contact_title -->
+				<?php if(get_field('att_bio_phone')):?>
 				
-				<span class="att_bio_contact">Betsy Berry</span><!-- att_bio_contact -->
+					<span class="att_bio_contact_title">Phone</span><!-- att_bio_contact_title -->
+				
+					<a class="att_bio_contact" href="tel:<?php echo str_replace(['-', '(', ')', ' '], '', get_field('att_bio_phone')); ?>"><?php the_field( 'att_bio_phone' ); ?></a><!-- att_bio_contact -->
+				
+				<?php endif;?>
+				
+				<?php if(get_field('att_bio_assistant')):?>
+				
+					<span class="att_bio_contact_title">Assistant</span><!-- att_bio_contact_title -->
+				
+					<span class="att_bio_contact"><?php the_field( 'att_bio_assistant' ); ?></span><!-- att_bio_contact -->
+				
+				<?php endif;?>
 				
 			</div><!-- att_bio_contact_info -->
+			
+			
+			<?php $post_object = get_field( 'recent_news_post' ); ?>
+			<?php if ( $post_object ): ?>
+			<?php $post = $post_object; ?>
+			<?php setup_postdata( $post ); ?> 
 			
 			<div class="recent_news_wrapper">
 				
 				<span class="recent_news_title">Recent News</span><!-- recent_news_title -->
 				
-				<span class="recent_news_post_title">Pedestrian Accidents</span><!-- recent_news_post_title -->
+				<span class="recent_news_post_title"><?php the_title();?></span><!-- recent_news_post_title -->
 				
-				<span class="recent_news_excerpt">Mr. Johnson has handled numerous pedestrian accidents throughout his career including cases involving serious traumatic. . .</span><!-- recent_news_excerpt -->
+				<span class="recent_news_excerpt">
 				
+					<?php echo wp_trim_words( get_the_content(), 15, '...' );?>
 				
-				<a class="sidebar_read_more" href="">Read More</a><!-- sidebar_read_more -->
+				</span><!-- recent_news_excerpt -->
+				
+				<a class="sidebar_read_more" href="<?php the_permalink();?>">Read More</a><!-- sidebar_read_more -->
 				
 			</div><!-- recent_news_wrapper -->
 			
-			<div class="att_bio_awards_wrapper">
+			<?php wp_reset_postdata(); ?>
+			<?php endif; ?>
+			
+			
+			
+			<?php if(get_field('attorney_bio_awards')): ?>
 				
-				<div class="att_bio_awards">
+					<div class="att_bio_awards_wrapper">
+						
+						<div class="att_bio_awards">
+			 
+				<?php while(has_sub_field('attorney_bio_awards')): ?>
+			 
+						<div class="att_bio_single_award">
+							
+							<?php $award_logo = get_sub_field( 'award_logo' ); ?>
+						
+							<img src="<?php echo $award_logo['url']; ?>" alt="<?php echo $award_logo['alt']; ?>" />
+						
+						</div><!-- att_bio_single_award -->
+			    
+				<?php endwhile; ?>
 					
-					<div class="att_bio_single_award">
+						</div><!-- att_bio_awards -->
 						
-						<img src="http://koonz-demo.com/wp-content/uploads/2019/02/award-johnson.png"/>
-						
-					</div><!-- att_bio_single_award -->
+						<div class="att_bio_button_wrapper">
 					
-					<div class="att_bio_single_award">
+							<div class="att_bio_button att_bio_button_left">
 						
-						<img src="http://koonz-demo.com/wp-content/uploads/2019/02/award-johnson.png"/>
+								<?php echo file_get_contents("wp-content/themes/koonz/images/arrow.svg"); ?>
 						
-					</div><!-- att_bio_single_award -->
+								<span>Prev</span>
+						
+							</div><!-- att_bio_button -->
 					
-					<div class="att_bio_single_award">
+							<div class="att_bio_button att_bio_button_right">
 						
-						<img src="http://koonz-demo.com/wp-content/uploads/2019/02/award-johnson.png"/>
+								<span>Next</span>
 						
-					</div><!-- att_bio_single_award -->
+								<?php echo file_get_contents("wp-content/themes/koonz/images/arrow.svg"); ?>
+						
+							</div><!-- att_bio_button -->
 					
-					<div class="att_bio_single_award">
-						
-						<img src="http://koonz-demo.com/wp-content/uploads/2019/02/award-johnson.png"/>
-						
-					</div><!-- att_bio_single_award -->
-					
-				</div><!-- att_bio_awards -->
-				
-				<div class="att_bio_button_wrapper">
-					
-					<div class="att_bio_button att_bio_button_left">
-						
-						<?php echo file_get_contents("wp-content/themes/koonz/images/arrow.svg"); ?>
-						
-						<span>Prev</span>
-						
-					</div><!-- att_bio_button -->
-					
-					<div class="att_bio_button att_bio_button_right">
-						
-						<span>Next</span>
-						
-						<?php echo file_get_contents("wp-content/themes/koonz/images/arrow.svg"); ?>
-						
-					</div><!-- att_bio_button -->
-					
-				</div><!-- att_bio_button_wrapper -->
-				
-			</div><!-- att_bio_awards_wrapper -->
+						</div><!-- att_bio_button_wrapper -->
+
+				</div><!-- att_bio_awards_wrapper -->
+			 
+			<?php endif; ?>
 			
 		</div><!-- att_bio_content -->
 		
